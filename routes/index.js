1cacef1af849b826db845248629cc06bd8736562
Nattanon Yanil <nattanonyanil@gmail.com>
2017-10-11 23:16:55 +0700

    first draft

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/Page1', function(req, res, next) {
+  res.render('Game1.ejs');
 });
+
+
 
 module.exports = router;
