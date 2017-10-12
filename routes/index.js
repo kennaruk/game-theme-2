e6f5b0090c852c5051dae7ed3c99235c15e0b7c5
Nattanon Yanil <nattanonyanil@gmail.com>
2017-10-12 21:09:41 +0700

    UIGameFinalV1

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ router.get('/Page1', function(req, res, next) {
   res.render('Game1.ejs');
 });
 
+router.get('/Page2', function(req, res, next) {
+  res.render('Game2.ejs');
+});
 
+router.get('/Page3', function(req, res, next) {
+  res.render('Game3.ejs');
+});
+
+router.get('/Page4', function(req, res, next) {
+  res.render('Game4.ejs');
+});
 
 module.exports = router;
